1cc0284f48b66d2ee3d27ac951da767e1c831e23
Iskariot <blablaname4@gmail.com>
2025-05-14 14:00:36 +0300

    added a provider to detect the device type.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from 'next/font/google';
+
 import './globals.css';
+import { BodyClassProvider } from '@/services/BodyClassProvider';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -19,7 +21,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+            <body className={`${geistSans.variable} ${geistMono.variable}`}>
+                <BodyClassProvider />
+                {children}
+            </body>
         </html>
     );
 }
---
+++ services/BodyClassProvider.jsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive';
+
+export function BodyClassProvider() {
+    const isDesktop = useMediaQuery({ minWidth: 767 });
+
+    useEffect(() => {
+        const bodyClassList = document.body.classList;
+        if (isDesktop) {
+            bodyClassList.contains('mobile') && bodyClassList.remove('mobile');
+            !bodyClassList.contains('desktop') && bodyClassList.add('desktop');
+        } else {
+            bodyClassList.contains('desktop') && bodyClassList.remove('desktop');
+            !bodyClassList.contains('mobile') && bodyClassList.add('mobile');
+        }
+    }, [isDesktop]);
+
+    return null;
+}
