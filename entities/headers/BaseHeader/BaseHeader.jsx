b36185b62f06accdac499b792ef8516a3d1e8a1a
Iskariot <blablaname4@gmail.com>
2025-05-14 14:03:28 +0300

    - refactored base header - styles have been split by folders - added burger menu - added adaptive

--- entities/headers/BaseHeader/BaseHeader.jsx
+++ entities/headers/BaseHeader/BaseHeader.jsx
@@ -1,49 +1,57 @@
+'use client';
+
 import Link from 'next/link';
 import st from './BaseHeader.module.scss';
+import cuid from 'cuid';
+import { useState } from 'react';
 
 const menuItems = Array.from({ length: 4 }, (_, index) => ({
     title: `Item ${index + 1}`,
-    depth: 0,
     submenu: Array.from({ length: 4 }, (_, subIndex) => ({
         title: `Subitem ${subIndex + 1}`,
-        depth: 1,
-        submenu: Array.from({ length: 4 }, (_, subSubIndex) => ({
-            title: `Subsubitem ${subSubIndex + 1}`,
-            depth: 2,
-            link: '#',
-        })),
+        link: '#',
     })),
 }));
 
-function RenderMenuList(item) {
-    const hasSubmenu = Array.isArray(item.submenu) && item.submenu.length;
-    const RenderTitle = (item) =>
-        item.link ? <Link href={item.link}>{item.title}</Link> : <span>{item.title}</span>;
-    return (
-        <li className={st[`depth-${item.depth}`]}>
-            <RenderTitle {...item} />
-            {hasSubmenu && (
-                <ul>
-                    {item.submenu.map((subItem, i) => (
-                        <RenderMenuList key={i} {...subItem} />
-                    ))}
-                </ul>
-            )}
-        </li>
-    );
-}
-
 export default function BaseHeader() {
+    const [isBurgerOpen, setBurgerOpen] = useState(false);
+    const [openSubmenuIndex, setOpenSubmenuIndex] = useState(null);
+    const toggleSubmenu = (index) => {
+        setOpenSubmenuIndex((prev) => (prev === index ? null : index));
+    };
+
     return (
         <header className={st.header}>
             <Link href="#" className={st.logo} />
-            <nav className={st.menu}>
-                <ul className={st.list}>
-                    {menuItems.map((item, i) => (
-                        <RenderMenuList key={i} {...item} />
+            <nav className={`${st.menu} ${isBurgerOpen ? st.open : ''}`}>
+                <ul className={`${st.list} ${st['list-1']}`}>
+                    {menuItems.map((item) => (
+                        <li
+                            key={cuid()}
+                            onClick={() => toggleSubmenu(item.title)}
+                            className={`${st.listItem} ${st['listItem-1']}`}>
+                            <span>{item.title}</span>
+                            <ul
+                                className={`${st.list} ${st['list-2']} ${
+                                    openSubmenuIndex === item.title ? st.open : ''
+                                }`}>
+                                {item.submenu.map((subItem) => (
+                                    <li
+                                        key={cuid()}
+                                        className={`${st.listItem} ${st['listItem-2']}`}>
+                                        <Link href={subItem.link}>{subItem.title}</Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
                     ))}
                 </ul>
             </nav>
+            <div
+                onClick={() => setBurgerOpen(!isBurgerOpen)}
+                className={`${st.burgerContainer} ${isBurgerOpen ? st.open : ''}`}>
+                <div className={st.burgerButton} />
+            </div>
         </header>
     );
 }
