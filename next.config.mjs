2e3fa49381ef98a315f0c44be40828cabbfa5922
Iskariot <blablaname4@gmail.com>
2025-06-05 01:11:04 +0300

    refactor(next.config): remove image domain configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['picsum.photos'], // Add all external image domains here
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
