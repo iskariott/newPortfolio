17243cc49d7f37a24be8c32b26fceb6b99173749
Iskariot <blablaname4@gmail.com>
2025-07-06 17:23:30 +0300

    refactor(store): restructure card loading store with improved type definitions and persist options

--- entities/card/model/store.ts
+++ entities/card/model/store.ts
@@ -1,12 +1,22 @@
 import { CARD_LOADING_LABEL_REVERSED, TCARD_LOADING_VARIANT } from './card-loading-variant';
 import { create } from 'zustand';
+import { persist, PersistOptions } from 'zustand/middleware';
 
-interface CardLoadingState {
+type CardLoadingState = {
     variant: TCARD_LOADING_VARIANT;
     setVariant: (label: string) => void;
-}
+};
 
-export const useCardLoadingStore = create<CardLoadingState>((set) => ({
-    variant: 'simple',
-    setVariant: (label) => set({ variant: CARD_LOADING_LABEL_REVERSED[label] }),
-}));
+type CardLoadingPersist = PersistOptions<CardLoadingState>;
+
+export const useCardLoadingStore = create<CardLoadingState>()(
+    persist<CardLoadingState, [], [], CardLoadingPersist>(
+        (set) => ({
+            variant: 'simple',
+            setVariant: (label) => set({ variant: CARD_LOADING_LABEL_REVERSED[label] }),
+        }),
+        {
+            name: 'card-loading-storage',
+        },
+    ),
+);
